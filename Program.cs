a75b502e0cb35224068283b0d3e3087c1109fb04
Cesar2812 <cesarcerdacomputer@gmail.com>
2024-10-15 11:08:01 -0600

    Agregando tablas de multiplicar

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TablasMultiplicar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            while (a<=5) 
+            {
+                Console.WriteLine("Escriba el numero de la tabla");
+                var numero = Convert.ToInt32(Console.ReadLine());// aca lo que le digo es que para que se pueda ejecutar el  metodo ReadLine() solo puede almacenar texto que lo convierta a entero para poder leer la variable
+
+                Console.WriteLine("La tabla del " + numero + " es:");
+                for (int i = 0; i < 12; i++)
+                {
+                    Console.WriteLine(numero + "*" + (i + 1) + "= " + (numero * (i + 1)));
+                }
+               a++;
+            }
+            
+
+
+        }
+    }
+}
